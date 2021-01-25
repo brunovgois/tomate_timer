0e0f986dd6cff9f221e850a3f2895b82584cf6cd
Bruno Gois <brunovgois@gmail.com>
2021-01-25 14:29:56 -0300

    set project states main page & tracker list done

---
+++ lib/study_session/presenter/UI/tracker_list.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+import 'package:tomate_timer/study_session/presenter/controller/session_controller.dart';
+
+class TrackerList extends StatefulWidget {
+  TrackerList({Key key, @required SessionController this.controller})
+      : super(key: key);
+  final SessionController controller;
+
+  @override
+  _TrackerListState createState() => _TrackerListState();
+}
+
+class _TrackerListState extends State<TrackerList> {
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.findAllSessions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 500,
+      child: ValueListenableBuilder(
+        valueListenable: widget.controller.sessions,
+        builder: (context, value, child) {
+          return ListView.builder(
+              scrollDirection: Axis.vertical,
+              reverse: true,
+              shrinkWrap: true,
+              itemCount: value.length ?? 0,
+              itemBuilder: (context, index) {
+                var tracker = value[index];
+                return ListTile(
+                  title: Text('${tracker.title} --> ${tracker.duration}'),
+                  subtitle: Text('${tracker.date}'),
+                );
+              });
+        },
+      ),
+    );
+  }
+}
