741f1e14b756f9cff0e855bd218b2f757143dd66
Bruno Gois <brunovgois@gmail.com>
2021-02-02 16:15:52 -0300

    change elevatedButton layout

--- lib/app_widget..dart
+++ lib/app_widget..dart
@@ -6,8 +6,11 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tomate Timer',
-      theme:
-          ThemeData(primaryColor: Colors.redAccent, buttonColor: Colors.green),
+      theme: ThemeData(primaryColor: Colors.redAccent).copyWith(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+        primary: Colors.green,
+      ))),
       initialRoute: "/",
       navigatorKey: Modular.navigatorKey,
       onGenerateRoute: Modular.generateRoute,
