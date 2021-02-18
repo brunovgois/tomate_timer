156411c65e4781245825efbda63d6bc128e2f0c6
Bruno Gois <brunovgois@gmail.com>
2021-02-17 21:45:41 -0300

    widget test Sessions Page

---
+++ test/features/study_session/presenter/UI/sessions_page._test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tomate_timer/features/study_session/presenter/UI/sessions_page..dart';
+import 'package:tomate_timer/features/study_session/presenter/UI/widgets/tracker_form.dart';
+import 'package:tomate_timer/features/study_session/presenter/UI/widgets/tracker_list.dart';
+import 'package:tomate_timer/features/study_session/study_session_module.dart';
+
+void main() {
+  SessionsPage sessionsPage;
+  setUp(() {
+    sessionsPage = new SessionsPage();
+  });
+
+  initModule(StudySessionModule());
+
+  testWidgets('Session page has TrackerForm and TrackerList as children',
+      (tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: sessionsPage,
+      ),
+    );
+    final formFinder = find.byType(TrackerForm);
+    final listFinder = find.byType(TrackerList);
+
+    expect(formFinder, findsOneWidget);
+    expect(listFinder, findsOneWidget);
+  });
+}
